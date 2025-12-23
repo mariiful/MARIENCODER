9fbb732e340faeb98f59333f10f0d4bef23156b6
raii <fdcgaming69@gmail.com>
2025-12-22 20:47:07 -0600

    fix current conditions to use the proper wind direction mapping

--- generators/current.js
+++ generators/current.js
@@ -1,3 +1,25 @@
+const windMapping = {
+    'Calm': 0,
+    'N': 1,
+    'NNE': 2,
+    'NE': 3,
+    'ENE': 4,
+    'E': 5,
+    'ESE': 6,
+    'SE': 7,
+    'SSE': 8,
+    'S': 9,
+    'SSW': 10,
+    'SW': 11,
+    'WSW': 12,
+    'W': 13,
+    'WNW': 14,
+    'NW': 15,
+    'NNW': 16,
+    'Var': 17
+};
+
+
 export function generateCurrent(input) {
     const expiration = input.validTimeUtc + 3600; // 1 hour from observation time
     
@@ -16,7 +38,7 @@ b.feelsLike = ${input.temperatureFeelsLike}
 b.dewpoint = ${input.temperatureDewPoint}
 b.altimeter = ${input.pressureAltimeter}
 b.visibility = ${input.visibility}
-b.windDirection = ${input.windDirection}
+b.windDirection = ${windMapping[input.windDirectionCardinal] || 0}
 b.windSpeed = ${input.windSpeed}
 b.gusts = ${input.windGust || 'None'}
 b.windChill = ${input.temperatureWindChill || 'None'}
